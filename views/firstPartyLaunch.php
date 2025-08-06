d6d3a122525c668a8966ab62cc1c5ad34234df70
Seth Battis <sbattis@groton.org>
2025-08-06 16:17:53 -0400

    feat: improve user-facing explanation of permission request

--- views/firstPartyLaunch.php
+++ views/firstPartyLaunch.php
@@ -9,8 +9,14 @@
 
 <body>
     <div class="container">
-        <h1>First Party Launch</h1>
-        <button id="request">Close</button>
+        <h1>Partitioned Session Cookies</h1>
+        <p>In order for this app to work properly, it needs to be able to store information about the session that you are starting using <a href="https://en.wikipedia.org/wiki/Third-party_cookies" target="_blank">third party cookies</a>, a technology associated with user-tracking on the web. Your browser (correctly) <a href="https://www.cookiestatus.com/" target="_blank">restricts the use of these cookies,</a> so we need you to give permission for this app to use them. THis will take place in three steps:</p>
+        <ol>
+            <li style="text-decoration: line-through">Launch the app in a first-party context</li>
+            <li>Engage with the app in the first-party context</li>
+            <li>Give explict permission to the app to store partitioned third-party cookies.</li>
+        </ol>
+        <button id="request">Engage with the app in the first-party context</button>
     </div>
     <script type="text/javascript">
         document.getElementById('request').addEventListener('click', () => {
