0e99e26f19caec692f23bd5338d8ff4a0a502eb0
Seth Battis <sbattis@groton.org>
2025-08-06 15:54:02 -0400

    feat: add viewport config to views for mobile

--- views/requestStorageAccess.php
+++ views/requestStorageAccess.php
@@ -7,6 +7,8 @@ use GrotonSchool\Slim\LTI\PartitionedSession\Actions\ThirdPartyCookieAction;
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Request Storage Access</title>
 </head>
 
