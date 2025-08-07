ab1bc1c195f0173bef70edd13f15a384081f2347
Seth Battis <sbattis@groton.org>
2025-08-07 14:26:59 -0400

    feat: use `RouteBuilder::define()` to define cookie-negotiation routes
    Addresses at least of some of the concern raised in #1

---
+++ src/RouteBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrotonSchool\Slim\LTI\PartitionedSession;
+
+use GrotonSchool\Slim\LTI\PartitionedSession\Actions\FirstPartyLaunchAction;
+use GrotonSchool\Slim\LTI\PartitionedSession\Actions\RequestStorageAccessAction;
+use GrotonSchool\Slim\LTI\PartitionedSession\Actions\ThirdPartyCookieAction;
+use GrotonSchool\Slim\LTI\PartitionedSession\Actions\ValidateSessionAction;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface;
+
+class RouteBuilder
+{
+    public static function define(App $app)
+    {
+        return $app->group('/lti', function (RouteCollectorProxyInterface $session) {
+            $session->get('/third-party-cookies', ThirdPartyCookieAction::class);
+            $session->get('/first-party-launch', FirstPartyLaunchAction::class);
+            $session->get('/request-storage-access', RequestStorageAccessAction::class);
+            $session->get('/validate-session', ValidateSessionAction::class);
+        });
+    }
+}
